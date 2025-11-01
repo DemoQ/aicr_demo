835245842a3ace073440993398053b7a007a81e4
DemoQ <zhao681232@126.com>
2025-11-02 01:31:13 +0800

    Update editableEnabled value in Config.java

--- fluent-gen/src/main/java/io/kubernetes/client/fluent/Config.java
+++ fluent-gen/src/main/java/io/kubernetes/client/fluent/Config.java
@@ -15,7 +15,7 @@ package io.kubernetes.client.fluent;
 import io.sundr.builder.annotations.ExternalBuildables;
 
 @ExternalBuildables(
-    editableEnabled = 123123,
+    editableEnabled = 12312344444,
     generateBuilderPackage = true,
     builderPackage = "io.kubernetes.client.fluent",
     value = {"io.kubernetes.client.openapi.models"})
